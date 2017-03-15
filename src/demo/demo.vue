9eba86b1c659afacaded5c1ee439740314fce38d
jlian <872821547@qq.com>
2017-03-15 13:34:09 +0800

    add lottery

--- src/demo/demo.vue
+++ src/demo/demo.vue
@@ -7,15 +7,16 @@
     </div>
     <ul class="list">
       <router-link to="/demo/swiper" tag="li" class="arrow"><span>Swiper</span></router-link>
-      <router-link to="/demo/countUp" tag="li" class="arrow"><span>countUp</span></router-link>
-      <router-link to="/demo/datetime" tag="li" class="arrow"><span>datetime</span></router-link>
-      <router-link to="/demo/addressPicker" tag="li" class="arrow"><span>addressPicker</span></router-link>
-      <router-link to="/demo/selectPicker" tag="li" class="arrow"><span>selectPicker</span></router-link>
-      <router-link to="/demo/dialog" tag="li" class="arrow"><span>dialog</span></router-link>
-      <router-link to="/demo/switch" tag="li" class="arrow"><span>switch</span></router-link>
-      <router-link to="/demo/grid" tag="li" class="arrow"><span>grid</span></router-link>
-      <router-link to="/demo/button" tag="li" class="arrow"><span>button</span></router-link>
-      <router-link to="/demo/loading" tag="li" class="arrow"><span>loading</span></router-link>
+      <router-link to="/demo/lottery" tag="li" class="arrow"><span>Lottery</span></router-link>
+      <router-link to="/demo/countUp" tag="li" class="arrow"><span>CountUp</span></router-link>
+      <router-link to="/demo/datetime" tag="li" class="arrow"><span>Datetime</span></router-link>
+      <router-link to="/demo/addressPicker" tag="li" class="arrow"><span>AddressPicker</span></router-link>
+      <router-link to="/demo/selectPicker" tag="li" class="arrow"><span>SelectPicker</span></router-link>
+      <router-link to="/demo/dialog" tag="li" class="arrow"><span>Dialog</span></router-link>
+      <router-link to="/demo/switch" tag="li" class="arrow"><span>Switch</span></router-link>
+      <router-link to="/demo/grid" tag="li" class="arrow"><span>Grid</span></router-link>
+      <router-link to="/demo/button" tag="li" class="arrow"><span>Button</span></router-link>
+      <router-link to="/demo/loading" tag="li" class="arrow"><span>Loading</span></router-link>
     </ul>
     <footer class="demo-index">© by J.Lian</footer>
   </div>
